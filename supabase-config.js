6452417f17fc3e652e01b64cbf1a8ad46676fbf9
GBRL BD <96059491+GBRL-BD@users.noreply.github.com>
2026-07-23 11:54:05 -0500

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.EMPRENDE_SUPABASE_URL='https://cbiapndsjlbgouuqvlbz.supabase.co';
-window.EMPRENDE_SUPABASE_KEY='sb_publishable_xg-pRfq7xGyPLuOFlNU-PQ_ZBlVfH9V';
-window.EMPRENDE_SUPABASE_CONFIG={url:window.EMPRENDE_SUPABASE_URL,anonKey:window.EMPRENDE_SUPABASE_KEY};
-window.emprendeSupabase=window.supabase?.createClient(window.EMPRENDE_SUPABASE_URL,window.EMPRENDE_SUPABASE_KEY)||null;
